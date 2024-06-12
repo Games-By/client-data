9b6727379ac027d279859fddeb1f8c734d562e09
Saviovd <savio.pinheiroal@gmail.com>
2024-06-12 10:01:54 -0400

    add: configure cors

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+   origin: 'http://localhost:3000',
+   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+   allowedHeaders: ['Content-Type', 'Authorization'],
+   credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 const AuthRegisterUserRoutes = require('./routes/AuthRegisterUserRoutes');
 const AuthLoginRoutes = require('./routes/AuthLoginRoutes');
