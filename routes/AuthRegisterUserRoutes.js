960d74035f07a2f73e69a8216e5d336222aec6c3
Saviovd <savio.pinheiroal@gmail.com>
2024-06-12 17:40:03 -0400

    fix: restore default

--- routes/AuthRegisterUserRoutes.js
+++ routes/AuthRegisterUserRoutes.js
@@ -1,26 +1,9 @@
 const router = require('express').Router();
 const multer = require('multer');
-const fs = require('fs-extra');
-const path = require('path');
-
-const uploadDir = path.join(__dirname, '../uploads');
-
-if (!fs.existsSync(uploadDir)) {
-   try {
-      fs.mkdirSync(uploadDir);
-   } catch (err) {
-      console.error('Error creating directory:', err);
-   }
-}
 
 const storage = multer.diskStorage({
-   destination: async function (req, file, cb) {
-      try {
-         await fs.ensureDir(uploadDir);
-         cb(null, uploadDir);
-      } catch (err) {
-         cb(err, uploadDir);
-      }
+   destination: function (req, file, cb) {
+      cb(null, 'uploads');
    },
    filename: function (req, file, cb) {
       cb(null, file.originalname);
@@ -31,7 +14,7 @@ const upload = multer({
    storage: storage,
    fileFilter: function (req, file, cb) {
       if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-         return cb(new Error('Only image files (png, jpg, jpeg, gif) are allowed!'));
+         return cb(new Error('Only image files (png, jpg) are allowed!'));
       }
       cb(null, true);
    }
@@ -39,12 +22,12 @@ const upload = multer({
 
 const AuthRegisterUserController = require('../controllers/AuthRegisterUserController');
 
+router.get('/', AuthRegisterUserController.init);
+router.post('/auth/register/user', AuthRegisterUserController.registerUser);
 router.post(
    '/upload/image',
    upload.single('image'),
    AuthRegisterUserController.uploadImage
 );
-router.get('/', AuthRegisterUserController.init);
-router.post('/auth/register/user', AuthRegisterUserController.registerUser);
 
 module.exports = router;
